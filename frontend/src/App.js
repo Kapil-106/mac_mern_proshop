a7bc7646c59128062fa6ff5eda2a316166eb6fdd
codework <waghelakapil@gmail.com>
2023-09-16 14:32:16 +0530

    frontend design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 
 import React from 'react'
-
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
-    <p>Welcome to sitaram</p>
+    <>
+      <Header></Header>
+      <main className='py-3'>
+         <Container>
+           <HomeScreen></HomeScreen>
+          </Container>
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
 
